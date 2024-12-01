52c158e76b98383729b8c9f067c6d5bacf419b98
re1n-e <raghavendrabargali@gmail.com>
2024-12-01 18:41:05 +0530

    codecrafters submit [skip ci]

--- src/command.rs
+++ src/command.rs
@@ -1,10 +1,28 @@
 use std::process::exit;
 
+fn handle_type(type_str: &str) {
+    match type_str {
+        "echo" => println!("echo is a shell builtin"),
+        "exit" => println!("exit is a shell builtin"),
+        "type" => println!("type is a shell builtin"),
+        "cat" => println!("cat is /bin/cat"),
+        _ => println!("{}: not found", type_str),
+    }
+}
+
 pub fn handle_cmd(command: &str) {
     if command.starts_with("echo") {
-        println!("{}", &command[5..]);
+        if command.len() > 4 {
+            println!("{}", command[5..].trim());
+        } else {
+            println!("");
+        }
     } else if command.starts_with("exit") {
         exit(0);
+    } else if command.starts_with("type") {
+        if command.len() > 4 {
+            handle_type(command[5..].trim());
+        }
     } else {
         println!("{}: command not found", command);
     }
