74d750dc319745a574a1c688ecc190912e05a4c7
re1n-e <raghavendrabargali@gmail.com>
2024-12-01 17:35:02 +0530

    [any message]

--- src/command.rs
+++ src/command.rs
@@ -1,5 +1,7 @@
 pub fn handle_cmd(command: &str) {
     match command {
-        _ => println!("{command}: command not found"),
+        _ => {
+            println!("{command}: command not found");
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,5 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
-    command::handle_cmd(&input);
+    command::handle_cmd(&input.trim());
 }
