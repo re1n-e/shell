b5b76aa084dcb4101e7b41ced422624b87c62e0b
re1n-e <raghavendrabargali@gmail.com>
2024-12-01 19:15:45 +0530

    codecrafters submit [skip ci]

--- src/command.rs
+++ src/command.rs
@@ -1,16 +1,25 @@
 use std::process::exit;
 
-fn handle_type(type_str: &str) {
+fn handle_type(type_str: &str, path_env: &str) {
     match type_str {
         "echo" => println!("echo is a shell builtin"),
         "exit" => println!("exit is a shell builtin"),
         "type" => println!("type is a shell builtin"),
         "cat" => println!("cat is /bin/cat"),
-        _ => println!("{}: not found", type_str),
+        _ => {
+            let split = &mut path_env.split(':');
+            if let Some(path) =
+                split.find(|path| std::fs::metadata(format!("{}/{}", path, type_str)).is_ok())
+            {
+                println!("{type_str} is {path}/{type_str}");
+            } else {
+                println!("{type_str} not found");
+            }
+        }
     }
 }
 
-pub fn handle_cmd(command: &str) {
+pub fn handle_cmd(command: &str, path_env: &str) {
     if command.starts_with("echo") {
         if command.len() > 4 {
             println!("{}", command[5..].trim());
@@ -21,7 +30,7 @@ pub fn handle_cmd(command: &str) {
         exit(0);
     } else if command.starts_with("type") {
         if command.len() > 4 {
-            handle_type(command[5..].trim());
+            handle_type(command[5..].trim(), path_env);
         }
     } else {
         println!("{}: command not found", command);
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
+    let path_env = std::env::var("PATH").unwrap();
     let mut input = String::new();
     print!("$ ");
     io::stdout().flush().unwrap();
     while io::stdin().read_line(&mut input).is_ok() {
-        command::handle_cmd(&input.trim());
+        command::handle_cmd(&input.trim(), &path_env);
         input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
