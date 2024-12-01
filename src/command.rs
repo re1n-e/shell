3d7b53ff811f550c6a989901f584a0f8c70fb0ff
re1n-e <raghavendrabargali@gmail.com>
2024-12-01 18:03:14 +0530

    codecrafters submit [skip ci]

--- src/command.rs
+++ src/command.rs
@@ -1,5 +1,10 @@
+use std::process::exit;
+
 pub fn handle_cmd(command: &str) {
     match command {
+        "exit 0" => {
+            exit(0);
+        }
         _ => {
             println!("{command}: command not found");
         }
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,24 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    loop {
+    let mut input = String::new();
+    print!("$ ");
+    io::stdout().flush().unwrap();
+    while io::stdin().read_line(&mut input).is_ok() {
+        command::handle_cmd(&input.trim());
+        input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
-
-        // Wait for user input
-        let stdin = io::stdin();
-        let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
-        command::handle_cmd(&input.trim());
     }
+
+    // loop {
+    //     print!("$ ");
+    //     io::stdout().flush().unwrap();
+
+    //     // Wait for user input
+    //     let stdin = io::stdin();
+    //     let mut input = String::new();
+    //     stdin.read_line(&mut input).unwrap();
+    //     command::handle_cmd(&input.trim());
+    // }
 }
